20c76187587cd9746da6f6e1a14b8f0d55b60ebe
nick762 <nzhdanov@gmail.com>
2023-02-14 12:15:00 +0300

    Работа с ds2782
    Добавлена работа с ds2782, и еще какие то изменения

--- Send_auto_215x320.ino
+++ Send_auto_215x320.ino
@@ -1,6 +1,7 @@
 #include <max17320.h>
 #include <SBS.h>
 #include <max17215.h>
+#include <ds2782.h>
 
 #define CHARGE 5
 #define DISCHARGE 4
@@ -12,6 +13,7 @@
 SBS stnd;
 MAX17215 wire215; 
 MAX17320 wire320;
+DS2782 DS;
 
 String str = "";
 bool start = false;
@@ -44,9 +46,15 @@ void setup() {
     }else{
       delay(100);
       resetFunc();
+      type = 3;
+    }
+  }else{
+    if(DS._readAddress() == 104){
+      type = 4;
     }
   }
-  Serial.println(type);
+  //Serial.print("Type: ");
+  //Serial.println(type);
   cellNumber();
   Serial.println(MAX_VOLTAGE);
   Serial.println(MIN_VOLTAGE);
@@ -74,6 +82,10 @@ void loop() {
     }
       delay(400);
       break;
+    case 4:
+    {
+      str = "@" + DS.GetData()+"," + String(c_charging) + "," + String(c_discharging) + "," + String(charging) + "," + String(discharging) + "#";
+    }
   }  
 
   if (charging == true) {
@@ -92,6 +104,11 @@ void loop() {
         break;
       case 2:
         if ((wire320._readVoltage() >= MAX_VOLTAGE) && (wire320._readCurrent() <= MIN_CURRENT)) {  //4 элемента
+          digitalWrite(CHARGE, LOW);
+          charging = false;
+        }
+      case 4:
+        if ((DS._readVoltage() >= MAX_VOLTAGE) && (DS._readCurrent() <= MIN_CURRENT)) {  //4 элемента
           digitalWrite(CHARGE, LOW);
           charging = false;
         }
@@ -115,6 +132,11 @@ void loop() {
         break;
       case 2:
         if ((wire320._readVoltage() <= MIN_VOLTAGE)) { //||(stnd.GetCap()<=1)) {  //4 элемента
+          digitalWrite(DISCHARGE, LOW);
+          discharging = false;
+        }
+      case 4:
+        if ((DS._readVoltage() <= MIN_VOLTAGE)) { //||(stnd.GetCap()<=1)) {  //4 элемента
           digitalWrite(DISCHARGE, LOW);
           discharging = false;
         }
@@ -169,6 +191,21 @@ void loop() {
             c_charging = false;
             c_discharging = true;
           } else if ((wire320._readVoltage() <= MIN_VOLTAGE) && (wire320._readCapacity() <= MIN_CAPACITY)) { //4 элемента
+            c_discharging = false;
+            c_charging = true;
+          }
+        case 4:
+          if ((DS._readVoltage() < MAX_VOLTAGE) && (DS._readCurrent() <= MIN_CURRENT) && (c_discharging == false)) { //4 элемента        
+            c_charging = true;
+            c_discharging = false;
+          } else if ((DS._readVoltage() >= MAX_VOLTAGE) && (DS._readCurrent() <= MIN_CURRENT)) { //4 элемента
+            c_charging = false;
+            c_discharging = true;
+          }
+          if ((DS._readVoltage() > MIN_VOLTAGE) && (DS._readCapacity() >= MIN_CAPACITY) && (c_charging == false)) {  //4 элемента
+            c_charging = false;
+            c_discharging = true;
+          } else if ((DS._readVoltage() <= MIN_VOLTAGE) && (DS._readCapacity() <= MIN_CAPACITY)) { //4 элемента
             c_discharging = false;
             c_charging = true;
           }
@@ -249,8 +286,8 @@ void loop() {
         }
         break;
       case 'f':
-        if(type == 0) stnd.FullAccess(); 
-        else if(type == 2) wire215._fReset();      
+        if(type == 0) stnd.FullAccess();
+        else if(type == 2) wire215._fReset();       
         break;
       case 'u':
         if(type == 0) stnd.Unseal();
@@ -277,6 +314,10 @@ void loop() {
         break;            
     }
   }
+  //if (sen){
+  //  Serial.println(str);
+  //  Serial.flush();
+  //}
 }
 
 void sendData() {
@@ -309,6 +350,16 @@ void sendData() {
     case 2:
       {
         String str = "@" + wire320.GetData();
+        Serial.print(str);
+      }
+      {
+        String str2 = "," + String(c_charging) + "," + String(c_discharging) + "," + String(charging) + "," + String(discharging) + "#";
+        Serial.println(str2);
+      }
+      Serial.flush(); 
+    case 4:
+      {
+        String str = "@" + DS.GetData();
         Serial.print(str);
       }
       {
@@ -401,6 +452,10 @@ void cellNumber(){
         Serial.println("2 cells");
         MAX_VOLTAGE = 8250;
         MIN_VOLTAGE = 5900;
+      }else if(stnd.readCellsNum() == 3){
+        Serial.println("4 cells");
+      // MAX_VOLTAGE = 12350;
+      // MIN_VOLTAGE = 11970;      
       }
       break;
     case 1: 
@@ -412,10 +467,13 @@ void cellNumber(){
         Serial.println("2 cells");
         MAX_VOLTAGE = 8250;
         MIN_VOLTAGE = 5900;      
-        //MIN_VOLTAGE = 7200; 
       }else if(wire215._cellNum() == 3){        
         Serial.println("3 cells");
       // MAX_VOLTAGE = 16550;
+      // MIN_VOLTAGE = 14260;
+      }else if(wire215._cellNum() == 4){
+        Serial.println("4 cells");
+        // MAX_VOLTAGE = 16550;
       // MIN_VOLTAGE = 14260;
       }
       break;
@@ -424,12 +482,18 @@ void cellNumber(){
         Serial.println("2 cells");
         MAX_VOLTAGE = 8250;
         MIN_VOLTAGE = 5900;      
-        //MIN_VOLTAGE = 7200;      
       }else if(wire320._cellNum() == 3){        
         Serial.println("3 cells");
       // MAX_VOLTAGE = 16550;
       // MIN_VOLTAGE = 14260;
+      }else if(wire320._cellNum() == 4){
+        Serial.println("4 cells");
+        // MAX_VOLTAGE = 16550;
+      // MIN_VOLTAGE = 14260;
       }
+    case 4:
+      MIN_VOLTAGE = 11000;
+      MAX_VOLTAGE = 16650;
       break;
   }
 }
